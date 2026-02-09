d6e03f6fe779cbd9233d71db1725ef88e9a0fd44
Deiver Julio <deiver-jc@live.com>
2026-02-09 11:37:20 -0500

    chore(config): update dependencies and vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://back-services.api-reservat.com',
+      "/api": {
+        target: "http://localhost:8000",
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path,
@@ -19,7 +19,7 @@ export default defineConfig({
   },
   build: {
     // Usa Terser para eliminar todos los console.* y debugger en el bundle de producción
-    minify: 'terser',
+    minify: "terser",
     terserOptions: {
       compress: {
         drop_console: true,
